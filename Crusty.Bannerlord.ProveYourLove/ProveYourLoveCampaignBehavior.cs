9137f62a4e48e22caaa5787996310c898e211ce0
Benjamin A. Cook <bacook31775@gmail.com>
2021-11-17 01:09:04 -0500

    ProveYourLove is basically working

--- Crusty.Bannerlord.ProveYourLove/ProveYourLoveCampaignBehavior.cs
+++ Crusty.Bannerlord.ProveYourLove/ProveYourLoveCampaignBehavior.cs
@@ -1,74 +1,61 @@
-using Helpers;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Localization;
-using System.Collections.Generic;
+using System;
+using TaleWorlds.Core;
+using System.Linq;
+using Helpers;
+using TaleWorlds.CampaignSystem.Actions;
 
 namespace Crusty.Bannerlord.ProveYourLove
 {
     class ProveYourLoveCampaignBehavior : CampaignBehaviorBase
     {
-        int conscienceCounter = 0;
-        bool IsBad
-        {
-            get
-            {  
-                return true;
-            }
-        }
-        bool IsGood
-        {
-            get
-            {  
-                return true;
-            }
-        }
+        protected bool QuestInitiatedForIssueOwner = false;
+        private bool _questIsAccepted = false;
+
+        public bool QuestIsAccepted { get => _questIsAccepted; private set => _questIsAccepted = value; }
 
         public override void RegisterEvents()
         {
-            CampaignEvents.SetupPreConversationEvent.AddNonSerializedListener(this, SetupPreConversation);
-            CampaignEvents.OnConversationEndedEvent.AddNonSerializedListener(this, OnConversationEnded);
-        }
-        private void OnConversationEnded()
-        {
-            Hero hero = new Hero();
-            if(Hero.OneToOneConversation != null) {hero = Hero.OneToOneConversationHero;}
-            else if(MobileParty.ConversationParty.LeaderHero != null) {hero = MobileParty.ConversationParty.LeaderHero;}
-            else {return;}
+            CampaignEvents.ConversationEnded.AddNonSerializedListener(this, OnConversationEnded);
             
-            if (Romance.GetRomanticLevel(Hero.MainHero, hero) == Romance.RomanceLevelEnum.FailedInCompatibility 
-                | Romance.GetRomanticLevel(Hero.MainHero, hero) == Romance.RomanceLevelEnum.FailedInCompatibility)
-            {
-                new ProveYourLoveCampaignBehavior.ProveYourLoveIssue(hero)
-            }
+            
             
         }
-        private void SetupPreConversation()
+
+        private void OnConversationEnded(CharacterObject characterObject)
         {
-        
-        Hero quest_giver = MobileParty.ConversationParty.LeaderHero;
-        new honor = quest_giver.GetHeroTraits().Honor;
-        new mercy = quest_giver.GetHeroTraits().Mercy;
-        new valor = quest_giver.GetHeroTraits().Valor;
-        new genorosity = quest_giver.GetHeroTraits().Genorosity;
-        conscienceCounter = 0 + honor + mercy + valor + genorosity;
-        conscienceCounter < 0 ? IsBad : IsGood; 
-        
-            try
+            Hero questGiver = characterObject.HeroObject;
+
+            if (Romance.GetRomanticLevel(Hero.MainHero, characterObject.HeroObject) == Romance.RomanceLevelEnum.FailedInCompatibility
+                | Romance.GetRomanticLevel(Hero.MainHero, characterObject.HeroObject) == Romance.RomanceLevelEnum.FailedInCompatibility)
+
             {
-                if (Romance.GetRomanticLevel(Hero.MainHero, MobileParty.ConversationParty.LeaderHero) == Romance.RomanceLevelEnum.FailedInCompatibility
-                | Romance.GetRomanticLevel(Hero.MainHero, MobileParty.ConversationParty.LeaderHero) == Romance.RomanceLevelEnum.FailedInPracticalities)
 
-                {
-                    Hero quest_giver = MobileParty.ConversationParty.LeaderHero;
-                    if (MobileParty.ConversationParty.LeaderHero != null)
-                    {
-                        new ProveYourLoveQuest(quest_giver).StartQuest();
-                    }
-                }
+                new ProveYourLoveQuest(questGiver);
+                DialogFlow prove_your_love_start = DialogFlow.CreateDialogFlow("lord_start_courtship_response", int.MaxValue)
+                    .NpcLine(new TextObject("Actually, after thinking it for a while, there is something you can do that may help prove yourself to me"))
+                        .Condition(() => Hero.OneToOneConversationHero == questGiver)
+                    .PlayerLine(new TextObject("What can I do?"))
+                    .NpcLine(new TextObject("My clan's bastard leader, {QUEST_GIVER_CLAN_LEADER}, has always had it in for me. If it weren't for them, " +
+                                            "I'd marry you this instant. If only some tragic incident were to befall them...then nothing could stop us from being together")
+                        .SetTextVariable("QUEST_GIVER_CLAN_LEADER", questGiver.Clan.Leader.Name))
+                    .BeginPlayerOptions()
+                        .PlayerOption(new TextObject("Say no more, I'll look into it"))
+                                .Consequence(() => new ProveYourLoveQuest(questGiver).StartQuest())
+                            .NpcLine(new TextObject("That is wonderful my love! If you succeed, I'll know you are willing to do what it takes to have me!"))
+                                .Consequence(() => QuestIsAccepted = true)
+                        .CloseDialog()
+                        .PlayerOption(new TextObject("I must consider this for some time. I'll come back later"))
+                            .NpcLine(new TextObject("Well don't ponder it for too long, there are plenty of other suitors who would surely be willing"))
+                        .CloseDialog()
+                    .EndPlayerOptions()
+                    .CloseDialog();
+                Campaign.Current.ConversationManager.AddDialogFlow(prove_your_love_start, this);
+
             }
-            catch { return; }
-        }
 
+        }
         public override void SyncData(IDataStore dataStore)
         {
 
@@ -76,36 +63,60 @@ namespace Crusty.Bannerlord.ProveYourLove
 
         internal class ProveYourLoveQuest : QuestBase
         {
+            private Hero _ClanLeader
+            {
+                get
+                {
+                    Hero hero = QuestGiver.Clan.Leader;
+                    return hero;
+                }
+            }
+            protected override void RegisterEvents()
+            {
+                CampaignEvents.HeroKilledEvent.AddNonSerializedListener(this, heroKilled);
+                
+            }
+
+            private void heroKilled(Hero arg1, Hero arg2, KillCharacterAction.KillCharacterActionDetail arg3, bool arg4)
+            {
+                if (_ClanLeader.IsDead)
+                {
+                    AddLog(_HeroKilledLog);
+                }
+            }
+
+            private TextObject _HeroKilledLog
+            {
+                get
+                {
+                    TextObject textObject = new TextObject("{CLAN_LEADER} has died. Go tell your lover the good news!");
+                    textObject.SetTextVariable("CLAN_LEADER", QuestGiver.Clan.Leader.Name);
+                    return textObject;
+                }
+            }
             private TextObject _StartQuestLog
             {
                 get
                 {
-                    TextObject test = new TextObject("TEST");
-                    return test;
+                    TextObject textObject = new TextObject("{CLAN_LEADER} must die. Find a way for that to happen");
+                    textObject.SetTextVariable("CLAN_LEADER", QuestGiver.Clan.Leader.Name);
+                    return textObject;
                 }
             }
 
-
-            public override TextObject Title
+            public ProveYourLoveQuest(Hero questGiver) : base("prove_your_love_quest", questGiver, CampaignTime.DaysFromNow(30f), rewardGold: 0)
             {
-                get
-                {
-                    TextObject prove_your_love_title = new TextObject("Prove your love!");
-                    return prove_your_love_title;
-                }
-            }
+                SetDialogs();
+                AddLog(_StartQuestLog);
+           
+           }
 
-            public override bool IsRemainingTimeHidden
-            {
-                get
-                {
-                    return false;
-                }
-            }
+            public override TextObject Title => new TextObject("Prove Your Love to " + QuestGiver.Name);
 
-            public ProveYourLoveQuest(Hero questGiver) : base("prove_your_love_quest", questGiver, CampaignTime.DaysFromNow(30), rewardGold: 0)
-            {
-            }
+
+            public override bool IsRemainingTimeHidden => true;
+
+
             protected override void InitializeQuestOnGameLoad()
             {
                 SetDialogs();
@@ -113,30 +124,38 @@ namespace Crusty.Bannerlord.ProveYourLove
 
             protected override void SetDialogs()
             {
-                bool isDishonorable = false;
-                OfferDialogFlow = DialogFlow.CreateDialogFlow(QuestManager.QuestOfferToken, 110)
-                    .NpcLine(new TextObject("After thinking on it for a while, I've decided there is something you can do to prove yourself to me. Are you interested?"))
-                    .PlayerLine(new TextObject("What did you have in mind?"))
-                    .NpcLine(new TextObject("KILL MY DAD"))
-                        .Condition(() => QuestGiver.GetHeroTraits().Honor < 0)
-                        .Consequence(() => isDishonorable = true)
-                    .NpcLine(new TextObject("GIMME HORSE"))
-                        .Condition(() => QuestGiver.GetHeroTraits().Honor >= 0)
-
+                
+                DialogFlow prove_your_love_competed_success = DialogFlow.CreateDialogFlow("hero_main_options", int.MaxValue)
+                    .PlayerLine(new TextObject("It's about {QUEST_GIVER_CLAN_LEADER}...").SetTextVariable("QUEST_GIVER_CLAN_LEADER", _ClanLeader.Name))
+                    .Condition(() => _ClanLeader.IsDead)
+                    .NpcLine(new TextObject("Oh yes, did you take care of our little problem yet?" +
+                    " Or perhaps fortune smiled on us and took care of it for us?"))
                     .BeginPlayerOptions()
-                        .PlayerOption(new TextObject("Actually, I don't think I can do that."))                     
-                        .PlayerOption(new TextObject("I'll bring you their head, my love!"))
-                            .Condition(() => isDishonorable)
-                        .PlayerOption(new TextObject("I'll bring you their head, my love!"))
-                            .Condition(() => !isDishonorable)
-
-
+                        .PlayerOption(new TextObject("The fool is dead, we can elope at once!"))
+                                .Condition(() => QuestGiver.Clan.Leader.IsDead)
+                            .NpcLine(new TextObject("What wonderful news, let's go my love!"))
+                                .Consequence(() => CompleteQuestWithSuccess())
+                        .CloseDialog()
+                    .PlayerOption(new TextObject("Not not yet..."))
+                        .Condition(() => QuestGiver.Clan.Leader.IsAlive)
+                        .NpcLine(new TextObject("Hurry up with it! Don't you want to marry me?"))
+                    .CloseDialog()
+                    .EndPlayerOptions()
                     .CloseDialog();
 
-                DiscussDialogFlow = DialogFlow.CreateDialogFlow(QuestManager.QuestDiscussToken, 110)
-                    .NpcLine(new TextObject("Have you done what I've asked yet?"));
+
+
+                Campaign.Current.ConversationManager.AddDialogFlow(prove_your_love_competed_success, this);
+
+            }
+
+            protected override void OnStartQuest()
+            {
+                base.OnStartQuest();
             }
         }
+
+
     }
     public class ProveYourLoveCampaignBehaviorTypeDefiner : CampaignBehaviorBase.SaveableCampaignBehaviorTypeDefiner
     {
