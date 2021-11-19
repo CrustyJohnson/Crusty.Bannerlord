e6c313cb3145910cf2386b4d6311981178ab32ee
CrustyJohnson <92555756+CrustyJohnson@users.noreply.github.com>
2021-11-18 22:41:23 -0500

    Crusty Story Mode1.6.5.2

---
+++ Crusty.Bannerlord.YouAreAllowedToTalkToMe/SubModule.cs
@@ -0,0 +1,45 @@
+﻿using Crusty.Bannerlord.CrustyStoryMode.Behaviors;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Crusty.Bannerlord.YouAreAllowedToTalkToMe
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            base.OnGameStart(game, gameStarter);
+            if (!(game.GameType is Campaign))
+                return;
+            CampaignGameStarter campaignGameStarter = (CampaignGameStarter)gameStarter;
+            this.AddBehaviors(campaignGameStarter);
+
+
+        }
+
+        private void AddBehaviors(CampaignGameStarter campaignGameStarter)
+        {
+            campaignGameStarter.AddBehavior(new CrustyHeroPrisonerBehavior());
+            CrustyHelpers.OutMsg("CrustyHeroPrisonerBehavior Added!");
+
+        }
+    }
+}
