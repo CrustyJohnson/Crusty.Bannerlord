72cbe11bf9dd56eeec36b78240a6c5398629474b
CrustyJohnson <92555756+CrustyJohnson@users.noreply.github.com>
2021-11-22 08:51:09 -0500

    Working on iinitial map state and phase 1. Getting player to follow father party around as he transports her to betrothed while bandits chase.

---
+++ Crusty.Bannerlord.AWomansLot/Crusty.Bannerlord.AWomansLot/Behaviors/AWomansLotFirstPhaseBehavior.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace Crusty.Bannerlord.AWomansLot.Behaviors
+{
+    public class AWomansLotFirstPhaseBehavior : CampaignBehaviorBase
+    {
+        private Hero antagonist = Hero.AllAliveHeroes.FirstOrDefault(t =>
+            t != Hero.MainHero && t.PartyBelongedTo != null && t.Clan.IsMafia && t.Culture == Hero.MainHero.Culture);
+        private Hero betrothed = Hero.AllAliveHeroes.FirstOrDefault(t =>
+            t != Hero.MainHero && t.CanMarry() && !t.IsFemale);
+        private int phase;
+
+
+        public Settlement AntagonistCamp { get { return Helpers.SettlementHelper.FindRandomHideout(); } }
+
+        public Hero Antagonist { get => antagonist; private set => antagonist = value; }
+        public Hero Betrothed { get => betrothed; private set => betrothed = value; }
+        public int AWomansLotPhase { get => phase; set => phase = value; }
+
+        public override void RegisterEvents()
+        {
+            phase = 0;
+            AWomansLotPhase = 0;
+            CampaignEvents.OnCharacterCreationIsOverEvent.AddNonSerializedListener(this, CharacterCreationIsOver);
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, OnSessionLaunched);
+            CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, OnHourlyTick);
+            CampaignEvents.TickEvent.AddNonSerializedListener(this, Tick);
+        }
+
+        private void Tick(float obj)
+        {
+            MobileParty.MainParty.SetMoveGoToPoint(Hero.MainHero.Father.PartyBelongedTo.GetPosition2D + new Vec2(0.25f,0.25f));
+        }
+
+        private void OnHourlyTick()
+        {
+            if (AWomansLotPhase == 0)
+            {
+                
+
+                if (Hero.MainHero.Father.PartyBelongedTo.Party != null)
+                {
+                    Hero.MainHero.Father.PartyBelongedTo.Party.SetAsCameraFollowParty();
+                    
+                }
+                
+                
+                    Hero.MainHero.BattleEquipment.Horse.Clear();
+                    CharacterObject.PlayerCharacter.Equipment.Horse.Clear();
+                
+
+            }
+
+
+            foreach (Hero hero in Hero.AllAliveHeroes)
+            {
+                if (hero.PartyBelongedTo != null && (hero.Clan.IsMafia | hero.Clan.IsBanditFaction | hero.Clan.IsClanTypeMercenary))
+                {
+                    hero.PartyBelongedTo.SetMoveEngageParty(MobileParty.MainParty);
+                    if (!hero.Clan.IsAtWarWith(Clan.PlayerClan))
+                        TaleWorlds.CampaignSystem.Actions.DeclareWarAction.Apply(hero.Clan.MapFaction, Hero.MainHero.Clan.MapFaction);
+
+
+
+                }
+            }
+        }
+        
+
+
+
+        
+
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            starter.AddDialogLine("phase1_start", "start", "phase_one_a", "It's a dangerous world, especially for caravans loaded with all those valuables. How about you" +
+                "hand them over to us for safe keeping? Hahaha",
+                () => Hero.OneToOneConversationHero.Clan.IsMafia, () => Antagonist = Hero.OneToOneConversationHero, 110);
+
+            starter.AddDialogLine("phase1_start", "start", "phase_one_a", "It's a dangerous world, especially for caravans loaded with all those valuables. How about you" +
+                "hand them over to us for safe keeping? Hahaha",
+                () => Hero.OneToOneConversationHero == Antagonist, () => this.AWomansLotPhase = 1, 110);
+        }
+
+
+        private void CharacterCreationIsOver()
+        {
+            
+            Hero.MainHero.BattleEquipment.Horse.Clear();
+            Hero Betrothed = Hero.AllAliveHeroes.GetRandomElementWithPredicate(t => t != Hero.MainHero && t.CanMarry() && !t.IsFemale && t.Culture != Hero.MainHero.Culture);
+            this.Betrothed = Betrothed;
+            InformationManager.ShowInquiry(new InquiryData("A Woman's Lot",
+                "You have been betrothed to " + this.Betrothed.Name.ToString() + " As is the custom, you will be joining his clan. Your father and a few of his " +
+                "workers have packed a hanbdsome dowry into a caravan. Soon you shall set off with him to be delivered.",
+                true, false, "So be it.", null, () => InformationManager.HideInquiry(), null));
+            AWomansLotPhase = 0;
+            betrothed = Betrothed;
+            ItemRoster caravanItems = new ItemRoster();
+            caravanItems.AddToCounts(DefaultItems.Hides, 1000);
+            
+            MobileParty fatherParty = LordPartyComponent.CreateLordParty("dowry_train", Hero.MainHero.Father, Hero.MainHero.PartyBelongedTo.Position2D, spawnRadius: 1f, null);
+            fatherParty.SetCustomName(new TaleWorlds.Localization.TextObject("Dowry Train"));
+            fatherParty.AddElementToMemberRoster(Hero.MainHero.Mother.CharacterObject, 1);
+            fatherParty.Party.AddElementToMemberRoster(Game.Current.ObjectManager.GetObject<CharacterObject>("battanian_clanwarrior"), 10);
+
+            //DeclareWarAction.Apply(Antagonist.MapFaction, Hero.MainHero.MapFaction);
+            //TakePrisonerAction.Apply(Antagonist.PartyBelongedTo.Party, Hero.MainHero);
+            //EncounterManager.StartPartyEncounter(Antagonist.PartyBelongedTo.Party, MobileParty.MainParty.Party);
+
+
+
+            //Antagonist.PartyBelongedTo.SetMoveGoToSettlement(settlement);
+
+
+        }
+        public override void SyncData(IDataStore dataStore)
+        {
+
+        }
+    }
+}
--- Crusty.Bannerlord.StoryMode.AWomansLot/AWomansLot/SubModule.cs
+++ Crusty.Bannerlord.StoryMode.AWomansLot/AWomansLot/SubModule.cs
@@ -15,12 +15,8 @@ namespace Crusty.Bannerlord.StoryMode
             base.OnSubModuleLoad();
             TextObject coreContentDisabledReason = new TextObject("{=V8BXjyYq}Disabled during installation.");
 
-            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("CrustyStoryMode", new TextObject("Crusty's Story Mode", null), 
-                9990, (Action)(() => MBGameManager.StartNewGame(new AWomansLotGameManager())), (Func<(bool, TextObject)>)(() 
-                => (Module.CurrentModule.IsOnlyCoreContentEnabled, coreContentDisabledReason))));
-
             Module.CurrentModule.AddInitialStateOption(new InitialStateOption("CrustyStoryMode", new TextObject("A Woman's Lot", null), 
-                9990, (Action)(() => MBGameManager.StartNewGame(new AWomansLotGameManager())), (Func<(bool, TextObject)>)(() 
+                1337, (Action)(() => MBGameManager.StartNewGame(new AWomansLotGameManager())), (Func<(bool, TextObject)>)(() 
                 => (Module.CurrentModule.IsOnlyCoreContentEnabled, coreContentDisabledReason))));
 
         }
